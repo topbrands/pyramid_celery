56531125a4f930875031651aa624fbbeab5f0fcb
bstar <bb@topbrands.ru>
2013-12-05 15:02:13 +0400

    add support newest versions of celery (3.1.6 and later)

--- pyramid_celery/commands/__init__.py
+++ pyramid_celery/commands/__init__.py
@@ -5,11 +5,20 @@ from celery.app import default_app
 
 class CommandMixin(object):
     preload_options = []
+    conf  = None
+
 
     def setup_app_from_commandline(self, argv):
+        if argv is None:
+            argv = sys.argv
+        print argv
         if len(argv) < 2:
             print >> sys.stderr, 'No configuration file specified.'
-            sys.exit(1)
-        bootstrap(argv[1])
+            return argv
+        if not self.conf:
+            self.conf = argv.pop(1)
+            argv.insert(1, 'worker')
+        bootstrap(self.conf)
+
         self.app = default_app
-        return argv[:1] + argv[2:]
+        return argv
--- pyramid_celery/commands/celeryd.py
+++ pyramid_celery/commands/celeryd.py
@@ -1,6 +1,6 @@
 import sys
 from pyramid_celery.commands import CommandMixin
-from celery.bin.celeryd import WorkerCommand as BaseWorkerCommand
+from celery.bin.celery import CeleryCommand
 
 try:
     from celery.concurrency.processes.forking import freeze_support
@@ -8,8 +8,15 @@ except ImportError:  # pragma: no cover
     freeze_support = lambda: True  # noqa
 
 
-class WorkerCommand(CommandMixin, BaseWorkerCommand):
+
+class WorkerCommand(CommandMixin, CeleryCommand):
     preload_options = ()
+
+    def execute_from_commandline(self, argv=None):
+        if argv is None:
+            argv = self.setup_app_from_commandline(argv)
+        print "ARGV", argv
+        return CeleryCommand.execute_from_commandline(self, argv)
 
 
 def main():
